0104c646c774789bbb8d176705d2b5debd74cc91
zzzjx <zhaojingxiang@bupt.edu.cn>
2019-05-11 14:50:47 +0800

    second commit

---
+++ src/com/DesignModels/ProductAndCusumer/Consumer.java
@@ -0,0 +1,21 @@
+package com.DesignModels.ProductAndCusumer;
+
+public class Consumer implements Runnable {
+    private PublicBox box;
+    public Consumer(PublicBox box){
+        this.box=box;
+    }
+    @Override
+    public void run(){
+        for(int i=0;i<10;i++){
+            try
+            {
+                System.out.println("con:i"+i);
+                Thread.sleep(3000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            box.decrease();
+        }
+    }
+}
