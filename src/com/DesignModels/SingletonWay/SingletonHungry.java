7cda0249b3721c1ef83efe23cc7324e5788f77ab
zzzjx <zhaojingxiang@bupt.edu.cn>
2019-05-13 12:19:13 +0800

    update Adapter

---
+++ src/com/DesignModels/SingletonWay/SingletonHungry.java
@@ -0,0 +1,9 @@
+package com.DesignModels.SingletonWay;
+
+public class SingletonHungry {
+    private static SingletonHungry instance = new SingletonHungry();
+    private SingletonHungry(){}
+    public static SingletonHungry getInstance(){
+        return instance;
+    }
+}
---
+++ src/com/DesignModels/SingletonWay/SingletonSingleLock.java
@@ -0,0 +1,12 @@
+package com.DesignModels.SingletonWay;
+
+public class SingletonSingleLock {
+    private static SingletonSingleLock instance = null;
+    private SingletonSingleLock(){}
+    public static synchronized SingletonSingleLock getInstance(){
+        if(instance==null) {
+            instance = new SingletonSingleLock();
+        }
+        return instance;
+    }
+}
