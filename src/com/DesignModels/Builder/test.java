ac9c11777e06176116ff1f05ffba26e634159334
zzzjx <zhaojingxiang@bupt.edu.cn>
2019-06-09 22:07:32 +0800

    Revert "builder model updates"
    This reverts commit 56e648e02a819a67385ac41860b19e3e79a4398d.
    
    # Conflicts:
    #	src/com/DesignModels/Builder/builder.java

--- src/com/DesignModels/Builder/test.java
+++ src/com/DesignModels/Builder/test.java
@@ -1,19 +1,4 @@
 package com.DesignModels.Builder;
 
-import com.DesignModels.FactoryModel.Product;
-
-import java.util.List;
-
 public class test {
-    public static void main(String[] args){
-        builder builder = new builder(2,1);
-        List<Product> listA = builder.buildA();
-        List<Product> listB = builder.buildB();
-        for(Product p:listA){
-            p.getProduct("这是A");
-        }
-        for(Product p:listB){
-            p.getProduct("这是B");
-        }
-    }
 }
--- src/com/DesignModels/Main.java
+++ src/com/DesignModels/Main.java
@@ -12,5 +12,15 @@ public class Main {
     public native int hashCode();
 
     public static void main(String[] args) {
+        int[] a = {5,8,4,3,6,1,2,9};
+        int y = 2;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int x:a){
+            list.add(x^y);
+            System.out.println(list.hashCode());
+        }
+
+
+        
     }
 }
