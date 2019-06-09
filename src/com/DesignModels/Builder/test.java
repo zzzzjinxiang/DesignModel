ca68ed129d2b021e36e91388570c94a17c7a201b
zzzjx <zhaojingxiang@bupt.edu.cn>
2019-06-09 22:07:38 +0800

    builder model updates

--- src/com/DesignModels/Builder/test.java
+++ src/com/DesignModels/Builder/test.java
@@ -1,4 +1,19 @@
 package com.DesignModels.Builder;
 
+import com.DesignModels.FactoryModel.Product;
+
+import java.util.List;
+
 public class test {
+    public static void main(String[] args){
+        builder builder = new builder(2,1);
+        List<Product> listA = builder.buildA();
+        List<Product> listB = builder.buildB();
+        for(Product p:listA){
+            p.getProduct("this is A");
+        }
+        for(Product p:listB){
+            p.getProduct("this is B");
+        }
+    }
 }
