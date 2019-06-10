497c31e01f1e117147a15da9cba13eec56335c37
zzzjx <zhaojingxiang@bupt.edu.cn>
2019-06-10 23:10:15 +0800

    Factory model updates

--- src/com/DesignModels/FactoryModel/ProductA.java
+++ src/com/DesignModels/FactoryModel/ProductA.java
@@ -4,7 +4,7 @@ public class ProductA implements Product {
 
     @Override
     public void getProduct(String msg) {
-        if(msg.equals("A"))
+        if(msg.equals("AdapterModels"))
             System.out.println("execute "+ msg);
         else System.out.println("error product "+msg);
     }
